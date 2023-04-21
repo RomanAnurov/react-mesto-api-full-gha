36baac7a9d50a6dd7cab0744ceb6e78a82cf0ff0
Roman Anurov <anurovroman@yandex.ru>
2023-04-21 04:50:00 +0300

    Всё

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,21 +5,23 @@ const allowedCors = [
   'https://localhost:3000',
 ];
 
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
-
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
   }
-  if (req === 'OPTIONS') {
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    res.status(200).send();
+    return res.end();
   }
 
-  next();
+  return next();
 };
